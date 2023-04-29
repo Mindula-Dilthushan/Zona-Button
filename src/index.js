453fd49e7e049adb0236a9853bcec469a66ee2ea
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-04-29 10:46:38 +0530

    zona-default-button-relase

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/**
+ *
+ * App Name     :   Zona Button
+ * App Author   :   Mindula Dilthushan Manamperi
+ * Author Email :   minduladilthushan1@gmail.com
+ *
+ */
+import ZonaButton from "./ZonaButton";
+export default ZonaButton;
