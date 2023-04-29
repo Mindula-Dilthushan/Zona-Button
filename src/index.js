d5bb7cadf720a65387ea2cad4f49e18346026dab
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-04-29 11:10:10 +0530

    update

--- src/index.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * App Name     :   Zona Button
- * App Author   :   Mindula Dilthushan Manamperi
- * Author Email :   minduladilthushan1@gmail.com
- *
- */
-import ZonaButton from "./ZonaButton";
-export default ZonaButton;
