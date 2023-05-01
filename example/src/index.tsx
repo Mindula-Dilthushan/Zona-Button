6b191faf6e8e440590e8e9888b34df5227373691
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-05-01 13:01:08 +0530

    updates

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom/client'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-    <React.StrictMode>
-        <div>
-            <h2>Test Zero Button Package</h2>
-        </div>
-    </React.StrictMode>
+  <React.StrictMode>
+    <div>
+      <h2>Test Zero Button Package</h2>
+    </div>
+  </React.StrictMode>,
 )
