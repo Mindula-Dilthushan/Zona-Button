11d5c82ddbd954a7e6d6581b8726b155c6a74c2d
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-05-01 13:27:34 +0530

    release-v1.0.20

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,11 +1,15 @@
+// @ts-ignore
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+// @ts-ignore
+import ReactDOM from 'react-dom/client';
+import { ZeroButton } from 'zona-button'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <div>
       <h2>Test Zero Button Package</h2>
+      <ZeroButton/>
     </div>
   </React.StrictMode>,
 )
