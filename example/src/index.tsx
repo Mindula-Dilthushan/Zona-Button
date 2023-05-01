6404099bfeecedc541a0d418673f214dafaa7308
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-05-01 16:54:14 +0530

    update-index

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,14 +2,14 @@
 import React from 'react'
 // @ts-ignore
 import ReactDOM from 'react-dom/client'
-import { ZeroButton } from 'zona-button'
+// import { ZeroButton } from 'zona-button'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <div>
       <h2>Test Zero Button Package</h2>
-      <ZeroButton/>
+      {/*<ZeroButton />*/}
     </div>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
