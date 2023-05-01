1735547ff398c99649908e44c46a717b9e8090aa
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-05-01 13:06:17 +0530

    release-v1.0.18

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+/**
+ *
+ * App Name     :   Zona Button
+ * App Author   :   Mindula Dilthushan Manamperi
+ * Author Email :   minduladilthushan1@gmail.com
+ *
+ */
+import React from 'react'
+
+const ZeroButton = () => {
+  return (
+    <div>
+      <h1>Welcome Zero Button Package!</h1>
+    </div>
+  )
+}
+
+export default ZeroButton
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * App Name     :   Zona Button
+ * App Author   :   Mindula Dilthushan Manamperi
+ * Author Email :   minduladilthushan1@gmail.com
+ *
+ */
+import ZeroButton from './components/App'
+
+export { ZeroButton }
